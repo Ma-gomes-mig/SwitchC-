de20ed67927789423355553144d1356b9f2cd11d
Matheus gomes <83517467+Ma-gomes-mig@users.noreply.github.com>
2022-07-27 22:11:01 -0300

    Código Switch com C#

---
+++ Controle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            int escolha = int.Parse(Console.ReadLine());           
+
+            switch(escolha)
+            {                
+                case 1:
+                    Console.WriteLine("Voçê escolheu a opção 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Voçê escolheu a opção 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Voçê escolheu a opção 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Voçê escolheu a opção 4");
+                    break;
+
+                default:
+                    Console.WriteLine("Nenhuma das Opções foram escolhidas.");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
